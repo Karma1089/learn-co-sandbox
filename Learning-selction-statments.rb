bd5e3afbcd0d927569efcfcd3e4ee1de5fe0c8de
Karmelitta Henderson <karma24butts@gmail.com>
2020-02-18 06:13:50 +0000

    Automatically backed up by Learn

---
+++ Learning-selction-statments.rb
@@ -0,0 +1,15 @@
+run_code_inside = TRUE
+puts" Code beforeif...end"
+if run_code_inside
+  puts"code inside"
+end
+puts "Code after if...end"
+
+chance_of_rain = 0.2
+puts"Lets go outside!"
+if chance_of_rain >0.5
+  puts"Pack an umbrella"
+else
+  puts "Enjoy this fine day"
+end "Oh, and always waer sunscreen"
+
---
+++ learning-comments.rb
@@ -0,0 +1,8 @@
+#perform a constant expression evaluation
+3
+# Assign constant 3 to bare-word variable triangle_sides
+'triangle_sides'
+puts "joy to the world"
+puts "all the boys and girls"
+# puts "Joy to the fishes in the deep bleue sea"
+puts "Joy to you and me"
